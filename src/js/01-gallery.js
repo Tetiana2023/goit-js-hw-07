37bcdaaef2881944f59a15f51815c7270c94deb9
Tetiana2023 <drofaj18@gmail.com>
2023-01-02 16:05:50 +0200

    task - 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+
+const galleryNewEl = galleryItems.map(item => {
+    return ` <a class= "gallery__link" href= "${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+})
+.join("");
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryNewEl);
+console.log(galleryEl);
+
+

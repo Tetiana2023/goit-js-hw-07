1739e36114c79ab1feba5ed06d1823d6764f38b1
Tetiana2023 <drofaj18@gmail.com>
2023-01-02 23:21:01 +0200

    task - 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,7 +8,7 @@ const galleryEl = document.querySelector(".gallery");
 
 const galleryNewEl = galleryItems.map(item => {
     return ` <a class= "gallery__link" href= "${item.original}">
-    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
     </a>`
 })
 .join("");
@@ -16,4 +16,19 @@ const galleryNewEl = galleryItems.map(item => {
 galleryEl.insertAdjacentHTML('afterbegin', galleryNewEl);
 console.log(galleryEl);
 
+galleryEl.addEventListener('click', onGalleryElClick)
+ 
+function onGalleryElClick(event){
+    event.preventDefault();
 
+    if (event.target.nodeName !== "IMG"){
+     return;
+    }
+        // if (!event.target.classList.contains("gallery__image")
+       // {return})
+    const instance = basicLightbox.create(`
+    <img  src="${event.target.dataset.source}"  width="800" height="600">
+`)
+    
+instance.show()
+}

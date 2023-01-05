6145e4de317575e5d0e4d445001b2dbdea377950
Tetiana2023 <drofaj18@gmail.com>
2023-01-05 15:59:24 +0200

    task -02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+
+const galleryNewEl = galleryItems.map(item => {
+    return ` <a class= "gallery__link" href= "${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+})
+.join("");
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryNewEl);
+console.log(galleryEl);
+
+//galleryEl.addEventListener('click', onGalleryElClick)
+ 
+//function onGalleryElClick(event){
+    //event.preventDefault();
+
+   // if (event.target.nodeName !== "IMG"){
+    // return;
+   //}
+        // if (!event.target.classList.contains("gallery__image"))
+       // {return}
+   // const instance = basicLightbox.create(`
+    //<img  src="${event.target.dataset.source}"  width="800" height="600">
+//`)
+    
+//instance.show()
+//}
+const gallery = new SimpleLightbox('.gallery a', 
+{ captions: true,
+    captionsData: "alt",
+    captionDelay: 250,});

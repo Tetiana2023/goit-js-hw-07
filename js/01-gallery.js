391dcd96e458b70564bb8bfdaf8b6847969f96c2
Tetiana2023 <drofaj18@gmail.com>
2023-01-05 20:07:39 +0200

    last

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+
+const galleryNewEl = galleryItems.map(item => {
+    return ` <a class= "gallery__link" href= "${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>`
+})
+.join("");
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryNewEl);
+console.log(galleryEl);
+
+galleryEl.addEventListener('click', onGalleryElClick)
+ 
+function onGalleryElClick(event){
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG"){
+     return;
+    }
+        // if (!event.target.classList.contains("gallery__image"))
+       // {return}
+    const instance = basicLightbox.create(`
+    <img  src="${event.target.dataset.source}"  width="800" height="600">
+`)
+    
+instance.show()
+}
